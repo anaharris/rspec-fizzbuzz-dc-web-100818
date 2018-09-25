435afd49f764e41b300795d0ba82e06c71859dc2
Ana Harris <vana1990@gmail.com>
2018-09-25 23:10:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     return "Fizz"
-  elseif int % 5 == 0 
+  elsif int % 5 == 0 
     return "Buzz"
+  elsif int % 15 == 0 
+    return "FizzBuzz"
+  else return nil 
+  end
 end
